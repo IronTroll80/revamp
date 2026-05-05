5272e7316b91f0a5f097e451669bcb92032d2f54
IronTroll80 <oghenekomeemeni@gmail.com>
2026-05-05 22:21:41 +0200

    fix product page

--- app/components/productContent.tsx
+++ app/components/productContent.tsx
@@ -28,61 +28,64 @@ const blogs = [
 
 export default function ProductContent() {
     return (
-        <div className={styles.container}>
-            <p className = {styles.route}>Home/ Marketplace / Wines & Vintages / <span>4th street sweet red wine</span></p>
-            <div className={styles.productDetails}>
-                <div className={styles.imageSection}>
-                </div>
-                <div className={styles.infoSection}>
-                    <span className= {styles.category}>Wines</span>
-                    <h1 className= {styles.name}>4th Street Sweet Red Wine</h1>
-                    <p className={styles.price}>₦ 15,000</p>
-                    <h4 className= {styles.descriptionTitle}>Description</h4>
-                    <p className={styles.description}>A sweet red wine with a rich and fruity flavor profile, perfect for those who enjoy a sweeter taste. It pairs well with desserts and can be enjoyed on its own.</p>
-                    <div className= {styles.sizeOptions}>
-                        <h4>Size</h4>
-                        <div className={styles.sizes}>
-                            <button className={styles.sizeButton}>750ml</button>
-                            <button className={styles.sizeButton}>1.5L</button>
+        <>
+            <div className={styles.container}>
+                <p className = {styles.route}>Home/ Marketplace / Wines & Vintages / <span>4th street sweet red wine</span></p>
+                <div className={styles.productDetails}>
+                    <div className={styles.imageSection}>
+                    </div>
+                    <div className={styles.infoSection}>
+                        <span className= {styles.category}>Wines</span>
+                        <h1 className= {styles.name}>4th Street Sweet Red Wine</h1>
+                        <p className={styles.price}>₦ 15,000</p>
+                        <h4 className= {styles.descriptionTitle}>Description</h4>
+                        <p className={styles.description}>A sweet red wine with a rich and fruity flavor profile, perfect for those who enjoy a sweeter taste. It pairs well with desserts and can be enjoyed on its own.</p>
+                        <div className= {styles.sizeOptions}>
+                            <h4>Size</h4>
+                            <div className={styles.sizes}>
+                                <button className={styles.sizeButton}>750ml</button>
+                                <button className={styles.sizeButton}>1.5L</button>
+                            </div>
                         </div>
+                        <button className={styles.addToCartButton}>Add to Cart</button>
+                        <span className= {styles.addInfo} title= 'See Delivery Info'>Additional Info <LuInfo/> </span>
                     </div>
-                    <button className={styles.addToCartButton}>Add to Cart</button>
-                    <span className= {styles.addInfo} title= 'See Delivery Info'>Additional Info <LuInfo/> </span>
                 </div>
-            </div>
-            <div className= {styles.similarProducts}>
-                <SectionTitle title="See Similar products" description = "Fan of" link = '/products'  bold = '4th Street Sweet Red Wine?'/>
-                    <div className = {styles.productContainer}>
-                        {Array.from({ length: 4 }, (_, i) => (
-                        <ProductCard key={i} />
-                        ))}
-                    </div>
+                <div className= {styles.similarProducts}>
+                    <SectionTitle title="See Similar products" description = "Fan of" link = '/products'  bold = '4th Street Sweet Red Wine?'/>
+                        <div className = {styles.productContainer}>
+                            {Array.from({ length: 4 }, (_, i) => (
+                            <ProductCard key={i} />
+                            ))}
+                        </div>
+                </div>
+                
             </div>
             <div className = {styles.productBlog}>
-                <div className= {styles.productBlogContainer}>
-                    <div className= {styles.sectionTitle}>
-                        <div>
-                            <p>Learn more about your drinks.</p>
-                            <h2>The Quicklie Blog</h2>
-                            </div>
-                            <span><Link href={'/blogs'}>See all →</Link></span>
-                        </div>
-                    <div className= {styles.blogSection}>
-                        {
-                            blogs.map((blog, index)=>(
-                                <div className= {styles.blog} key={index}>
-                                    <div className= {styles.imageContainer}></div>
-                                    <p className= {styles.tag}>{blog.tag}</p>
-                                    <p className= {styles.title}>{blog.title}</p>
-                                    <p className= {styles.blogDescription}>{blog.description}</p>
-                                    <p className= {styles.meta}>{blog.meta}</p>
+                    <div className= {styles.productBlogContainer}>
+                        <div className= {styles.sectionTitle}>
+                            <div>
+                                <p>Learn more about your drinks.</p>
+                                <h2>The Quicklie Blog</h2>
                                 </div>
-                            ))
-                        }
+                                <span><Link href={'/blogs'}>See all →</Link></span>
+                            </div>
+                        <div className= {styles.blogSection}>
+                            {
+                                blogs.map((blog, index)=>(
+                                    <div className= {styles.blog} key={index}>
+                                        <div className= {styles.imageContainer}></div>
+                                        <p className= {styles.tag}>{blog.tag}</p>
+                                        <p className= {styles.title}>{blog.title}</p>
+                                        <p className= {styles.blogDescription}>{blog.description}</p>
+                                        <p className= {styles.meta}>{blog.meta}</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+        </>
         
     )
 }
